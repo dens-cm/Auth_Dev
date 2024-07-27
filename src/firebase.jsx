fdee9b29d97d253bac42f85b9362cd9e287a0b7f
Dens.CM <144193048+dens-cm@users.noreply.github.com>
2024-07-27 15:13:48 +0800

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 
 const app = firebase.initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBvHMl9nPvCXGp-dcyXkVf_GoAHgbF5kT4",
+  authDomain: "auth-development-3ac5e.firebaseapp.com",
+  projectId: "auth-development-3ac5e",
+  storageBucket: "auth-development-3ac5e.appspot.com",
+  messagingSenderId: "641217848062",
+  appId: "1:641217848062:web:3a773d229d164920abfbaf",
+  measurementId: "G-3GKMX97XXH",
 })
 
 export const auth = app.auth()
